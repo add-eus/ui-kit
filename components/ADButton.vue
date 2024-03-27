bdb4ef8a7331b0891bb1969b164eb8abd3699055
Loïc <loicbere@gmail.com>
2024-03-27 14:17:32 +0100

    feat: ADButton/ADIcon & Histoire doc - Regroup DS24-DS25-DS26

---
+++ components/ADButton.vue
@@ -0,0 +1,140 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: undefined,
+    validator: (value) => {
+        return ['primary', 'danger', 'success'].includes(value);
+    },
+  },
+});
+
+</script>
+
+<template>
+    <button :class="['ad-button', color]">
+      <slot></slot>
+    </button>
+</template>
+  
+<style scoped lang="scss">
+  .ad-button {
+    border: none;
+    border-radius: 50px;
+    padding: 3px 8px;
+    height: 35px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: background-color 0.15s ease-in-out, color 0.15s ease-in-out;
+    
+    //DEFAULT
+    background-color: var(--ad-grey-light);
+    color: var(--ad-black);
+
+    i {
+      color: var(--ad-grey-light);
+      transition: color 0.15s ease-in-out;
+    }
+
+    &:hover {
+      background-color: var(--ad-grey);
+      color: var(--ad-black);
+      i {
+        color: var(--ad-black);
+      }
+    }
+    
+    &:active {
+      background-color: var(--ad-black);
+      color: var(--ad-white);
+      i {
+        color: var(--ad-white);
+      }
+    }
+    
+    //PRIMARY
+    &.primary {
+      background-color: var(--ad-primary);
+      color: var(--ad-white);
+
+      i {
+        color: var(--ad-white);
+      }
+      
+      &:hover {
+        background-color: var(--ad-primary-light);
+        color: var(--ad-white);
+        i {
+          color: var(--ad-white);
+        }
+      }
+      
+      &:active {
+        background-color: var(--ad-white);
+        color: var(--ad-primary);
+        i {
+          color: var(--ad-primary);
+        }
+      }
+    }
+
+    //DANGER
+    &.danger {
+      background-color: var(--ad-danger);
+      color: var(--ad-white);
+
+      i {
+        color: var(--ad-white);
+      }
+
+      &:hover {
+        background-color: var(--ad-danger-light);
+        color: var(--ad-white);
+        i {
+          color: var(--ad-white);
+        }
+      }
+
+      &:active {
+        background-color: var(--ad-white);
+        color: var(--ad-danger);
+        i {
+          color: var(--ad-danger);
+        }
+      }
+    }
+    
+    //SUCCESS
+    &.success {
+      background-color: var(--ad-success);
+      color: var(--ad-white);
+
+      i {
+        color: var(--ad-white);
+      }
+
+      &:hover {
+        background-color: var(--ad-success-light);
+        color: var(--ad-white);
+        i {
+          color: var(--ad-white);
+        }
+      }
+
+      &:active {
+        background-color: var(--ad-white);
+        color: var(--ad-success);
+        i {
+          color: var(--ad-success);
+        }
+      }
+    }
+
+    
+  }
+  
+</style>
---
+++ components/ADIcon.story.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import ADIcon from "./ADIcon.vue";
+import '../scss/_color.scss'; 
+</script>
+
+<template>
+    <Story :layout="{ type: 'grid', width: 200 }">
+        <Variant title="default">
+            <ADIcon />
+        </Variant>
+        <Variant title="primary ">
+            <ADIcon icon="arrow_forward" type="rounded" color="var(--ad-primary)"/>
+        </Variant>
+        <Variant title="danger">
+            <ADIcon icon="close" type="sharp" color="var(--ad-danger)"/>
+        </Variant>
+        <Variant title="success">
+            <ADIcon icon="check" type="outlined" color="var(--ad-success)"/>
+        </Variant>
+    </Story>
+</template>
+
+<docs lang="md">
+# ADIcon
+
+This is a icon component.
+
+## Usage
+
+The `ADIcon` component provides the following options:
+
+- `color` (string, optional): Specifies the color of the button. Possible values are any valid color or variable like `var(--ad-primary)`.
+- `icon` (string, optional): Specifies the icon to be displayed inside the button. Possible values are any valid icon name.
+- `type` (string, optional): Specifies the type of the icon. Possible values are `rounded`, `outlined`, or `sharp`.
+</docs>
