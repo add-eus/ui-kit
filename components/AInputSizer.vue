378e51a324fb9700c638eb4129298a420d432121
Loïc <loicbere@gmail.com>
2024-05-28 15:28:21 +0200

    feat: add AInputSizer

---
+++ components/AInputSizer.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { useFocus } from "@vueuse/core";
+import { computed, ref, watch } from "vue";
+
+export interface AInputSizerEmits {
+  (event: "update:modelValue", value?: any): void;
+}
+export interface AInputSizerProps {
+  raw?: boolean;
+  modelValue?: any;
+  disabled?: boolean;
+}
+
+const emits = defineEmits<AInputSizerEmits>();
+const props = withDefaults(defineProps<AInputSizerProps>(), { modelValue: "" });
+const value = ref(props.modelValue);
+
+const textarea = ref<HTMLTextAreaElement | null>(null);
+const { focused } = useFocus(textarea);
+
+watch(value, () => {
+  emits("update:modelValue", value.value);
+});
+watch(
+  () => props.modelValue,
+  () => {
+    value.value = props.modelValue;
+  }
+);
+
+const classes = computed(() => {
+  if (props.raw) return [];
+
+  return ["textarea"];
+});
+
+defineExpose({
+  focused,
+  textarea,
+});
+</script>
+
+<template>
+  <div class="a-input-sizer a-input-sizer-stacked" :data-value="value">
+    <textarea
+      ref="textarea"
+      v-model="value"
+      :class="classes"
+      :disabled="disabled"
+      oninput="parentNode.dataset.value = value"
+      rows="1"
+      placeholder="hi"
+    ></textarea>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.a-input-sizer {
+  display: inline-grid;
+  vertical-align: top;
+  align-items: center;
+  position: relative;
+  border: 1px solid var(--a-grey-lighter);
+  border-radius: 4px;
+  padding: 0.25em 0.5em;
+
+  &.a-input-sizer-stacked {
+    padding: 0.5em;
+    align-items: stretch;
+
+    &::after,
+    input,
+    textarea {
+      grid-area: 2 / 1;
+    }
+  }
+
+  &::after,
+  input,
+  textarea {
+    width: auto;
+    min-width: 1em;
+    grid-area: 1 / 2;
+    font: inherit;
+    padding: 0.25em;
+    margin: 0;
+    resize: none;
+    background: none;
+    appearance: none;
+    border: none;
+    min-height: 80px;
+    /* max-height: 500px;
+    width: 85%; */
+  }
+
+  span {
+    padding: 0.25em;
+  }
+
+  &::after {
+    content: attr(data-value) " ";
+    visibility: hidden;
+    white-space: pre-wrap;
+  }
+
+  &:focus-within {
+    outline: solid 1px var(--a-grey);
+
+    textarea:focus,
+    input:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+}
+</style>
