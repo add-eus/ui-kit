cae73d01b6d1ec40c639fd18db324720e06e165c
Charles <charles.blancheton@gmail.com>
2024-07-10 16:56:10 +0200

    feat: add AInputTime

---
+++ components/AInputTime.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import moment, { type Moment } from "moment";
+import "v-calendar/style.css";
+import { computed, ref, watch } from "vue";
+import ASelect from "./ASelect.vue";
+
+export interface AInputTimeProps {
+  modelValue: Moment | undefined;
+}
+
+defineProps<AInputTimeProps>();
+
+const model = defineModel<Moment | undefined>({ default: undefined });
+const proxyModel = computed({
+  get: () => model.value,
+  set: (value: Moment | undefined) => {
+    model.value = value;
+  },
+});
+
+const hours = ref<number | undefined | null>(model.value?.hours());
+const minutes = ref<number | undefined | null>(model.value?.minutes());
+
+watch([hours, minutes], ([hours, minutes]) => {
+  if (
+    hours !== undefined &&
+    hours !== null &&
+    minutes !== undefined &&
+    minutes !== null
+  ) {
+    proxyModel.value = moment().hours(hours).minutes(minutes);
+  } else {
+    proxyModel.value = undefined;
+  }
+});
+watch(proxyModel, (value) => {
+  if (value !== undefined) {
+    hours.value = value.hours();
+    minutes.value = value.minutes();
+  } else if (
+    hours.value !== undefined &&
+    hours.value !== null &&
+    minutes.value !== undefined &&
+    minutes.value !== null
+  ) {
+    hours.value = undefined;
+    minutes.value = undefined;
+  }
+});
+
+const hoursOptions = Array.from({ length: 24 }, (_, i) => ({
+  value: i,
+  label: i.toString().padStart(2, "0"),
+}));
+const minutesOptions = Array.from({ length: 60 }, (_, i) => ({
+  value: i,
+  label: i.toString().padStart(2, "0"), 
+}));
+
+
+</script> 
+
+<template>
+  <div>
+    <ASelect mode="single" v-model="hours" :options="hoursOptions"> </ASelect>
+    <ASelect mode="single" v-model="minutes" :options="minutesOptions">
+    </ASelect>
+  </div>
+</template>
---
+++ stories/components/AInputTime.story.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import moment from "moment";
+import { markRaw, ref } from "vue";
+import AInputTime from "../../components/AInputTime.vue";
+import "../../index.scss";
+
+const time = ref(markRaw(moment()));
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="default">
+      <AInputTime v-model="time" />
+    </Variant>
+  </Story>
+</template>
+
+<docs lang="md">
+# AInputDate
+
+This is a input date component.
+
+## Usage
+
+The `AInputDate` component provides the following options:
+- `v-model` (moment): Specifies the model value. Possible values are any valid moment object.
+</docs>
