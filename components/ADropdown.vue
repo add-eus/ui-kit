be19d397885cacab7cd79e9e4ca445f43f279a24
Loïc <loicbere@gmail.com>
2024-09-09 16:41:16 +0200

    feat: update ADropdown

--- components/ADropdown.vue
+++ components/ADropdown.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import AButton from "./AButton.vue";
+import ACard from "./ACard.vue";
+import { useElementBounding, onClickOutside } from "@vueuse/core";
+
+const isOpen = defineModel("isOpen");
+
+const baseElement = ref(null);
+const boundingElement = useElementBounding(baseElement);
+
+const styleDropdown = computed(() => {
+  return {
+    top: `${boundingElement.bottom.value + window.scrollY}px`,
+    left: `${boundingElement.left.value + window.scrollX}px`,
+    width: `${boundingElement.width.value}px`,
+  };
+});
+
+onClickOutside(baseElement, () => {
+  isOpen.value = false;
+});
+
+const toggleDropdown = () => {
+  isOpen.value = !isOpen.value;
+};
+
+const openDropdown = () => {
+  isOpen.value = true;
+};
+
+const closeDropdown = () => {
+  isOpen.value = false;
+};
+</script>
+
 <template>
   <div class="a-dropdown" ref="baseElement">
     <slot
@@ -20,42 +56,6 @@
     </Teleport>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref, computed } from "vue";
-import AButton from "./AButton.vue";
-import ACard from "./ACard.vue";
-import { useElementBounding, onClickOutside } from "@vueuse/core";
-
-const isOpen = defineModel("isOpen");
-
-const baseElement = ref(null);
-const boundingElement = useElementBounding(baseElement);
-
-const styleDropdown = computed(() => {
-  return {
-    top: `${boundingElement.bottom.value}px`,
-    left: `${boundingElement.left.value}px`,
-    width: `${boundingElement.width.value}px`,
-  };
-});
-
-onClickOutside(baseElement, () => {
-  isOpen.value = false;
-});
-
-const toggleDropdown = () => {
-  isOpen.value = !isOpen.value;
-};
-
-const openDropdown = () => {
-  isOpen.value = true;
-};
-
-const closeDropdown = () => {
-  isOpen.value = false;
-};
-</script>
 
 <style lang="scss">
 .a-dropdown {
