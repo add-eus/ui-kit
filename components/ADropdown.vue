ee8d869ebf6cb3fb6ad93a5b01012dfa60460c91
Loïc <loicbere@gmail.com>
2024-09-10 17:48:11 +0200

    feat: update dropdown scroll actualisation

--- components/ADropdown.vue
+++ components/ADropdown.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import AButton from "./AButton.vue";
 import ACard from "./ACard.vue";
 import { useElementBounding, onClickOutside } from "@vueuse/core";
@@ -7,14 +7,31 @@ import { useElementBounding, onClickOutside } from "@vueuse/core";
 const isOpen = defineModel("isOpen");
 
 const baseElement = ref(null);
+const dropdownStyles = ref({
+  top: "0px",
+  left: "0px",
+  width: "0px",
+});
+
 const boundingElement = useElementBounding(baseElement);
 
-const styleDropdown = computed(() => {
-  return {
+const updateDropdownPosition = () => {
+  dropdownStyles.value = {
     top: `${boundingElement.bottom.value + window.scrollY}px`,
-    left: `${boundingElement.left.value + window.scrollX}px`,
+    left: `${boundingElement.left.value}px`,
     width: `${boundingElement.width.value}px`,
   };
+};
+
+// Mettre à jour la position du dropdown quand on ouvre et quand on fait scroll
+onMounted(() => {
+  updateDropdownPosition(); // Initialement, quand le dropdown s'affiche
+  window.addEventListener("scroll", updateDropdownPosition);
+});
+
+// Nettoyage des écouteurs lors de la destruction du composant
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", updateDropdownPosition);
 });
 
 onClickOutside(baseElement, () => {
@@ -23,10 +40,12 @@ onClickOutside(baseElement, () => {
 
 const toggleDropdown = () => {
   isOpen.value = !isOpen.value;
+  if (isOpen.value) updateDropdownPosition(); // Mise à jour quand on ouvre
 };
 
 const openDropdown = () => {
   isOpen.value = true;
+  updateDropdownPosition(); // Mise à jour quand on ouvre
 };
 
 const closeDropdown = () => {
@@ -46,7 +65,7 @@ const closeDropdown = () => {
     </slot>
 
     <Teleport to="body">
-      <ACard v-if="isOpen" class="a-dropdown-content" :style="styleDropdown">
+      <ACard v-if="isOpen" class="a-dropdown-content" :style="dropdownStyles">
         <template #content>
           <div class="flex-column">
             <slot name="content"></slot>
@@ -62,12 +81,21 @@ const closeDropdown = () => {
   position: relative;
   display: inline-block;
 }
+.a-dropdown-content {
+  .a-button {
+    display: flex;
+    justify-content: flex-start;
+    padding: 7px 8px;
+  }
+}
 
 .a-card {
   &.a-dropdown-content {
     position: absolute !important;
     max-height: none !important;
     min-width: 120px;
+    width: fit-content !important;
+    z-index: 100;
 
     > .a-card-body > .inner-content {
       padding: 0;
