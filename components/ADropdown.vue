2bb61b4b41059b18b9eae121d49566db42b42d15
Loïc <loicbere@gmail.com>
2024-09-11 15:39:52 +0200

    feat: update position props dropdown

--- components/ADropdown.vue
+++ components/ADropdown.vue
@@ -1,26 +1,57 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, onMounted, onBeforeUnmount, nextTick } from "vue";
 import AButton from "./AButton.vue";
 import ACard from "./ACard.vue";
 import { useElementBounding, onClickOutside } from "@vueuse/core";
 
+const props = defineProps({
+  position: {
+    type: String,
+    default: "left",
+    validator: (value: string) => {
+      return ["left", "right", "auto"].includes(value);
+    },
+  },
+});
+
 const isOpen = defineModel("isOpen");
 
-const baseElement = ref(null);
+const baseElement = ref<HTMLElement | null>(null);
+const buttonElement = ref<HTMLElement | null>(null);
 const dropdownStyles = ref({
   top: "0px",
   left: "0px",
   width: "0px",
+  transform: "",
 });
 
 const boundingElement = useElementBounding(baseElement);
 
-const updateDropdownPosition = () => {
+const updateDropdownPosition = async () => {
+  await nextTick();
+
+  const windowWidth = window.innerWidth;
+  const dropdownLeft = boundingElement.left.value + window.scrollX;
+
   dropdownStyles.value = {
     top: `${boundingElement.bottom.value + window.scrollY}px`,
-    left: `${boundingElement.left.value + window.scrollX}px`,
+    left: `${dropdownLeft}px`,
     width: `${boundingElement.width.value}px`,
+    transform: "",
   };
+
+  const buttonWidth = buttonElement.value
+    ? buttonElement.value.getBoundingClientRect().width
+    : 0;
+
+  // IF DROPDOWN IS NEXT TO THE SCREEN EDGE
+  if (props.position === "auto" && dropdownLeft > windowWidth / 1.25) {
+    dropdownStyles.value.transform = `translate(calc(-100% + ${buttonWidth}px))`;
+  }
+
+  if (props.position === "right") {
+    dropdownStyles.value.transform = `translate(calc(-100% + ${buttonWidth}px))`;
+  }
 };
 
 onMounted(() => {
@@ -53,14 +84,16 @@ const closeDropdown = () => {
 
 <template>
   <div class="a-dropdown" ref="baseElement">
-    <slot
-      name="action"
-      :toggle="toggleDropdown"
-      :open="openDropdown"
-      :close="closeDropdown"
-    >
-      <AButton @click="toggleDropdown()"></AButton>
-    </slot>
+    <div ref="buttonElement">
+      <slot
+        name="action"
+        :toggle="toggleDropdown"
+        :open="openDropdown"
+        :close="closeDropdown"
+      >
+        <AButton @click="toggleDropdown()"></AButton>
+      </slot>
+    </div>
 
     <Teleport to="body">
       <ACard v-if="isOpen" class="a-dropdown-content" :style="dropdownStyles">
@@ -79,6 +112,7 @@ const closeDropdown = () => {
   position: relative;
   display: inline-block;
 }
+
 .a-dropdown-content {
   .a-button {
     display: flex;
