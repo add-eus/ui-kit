a1263cfb25899c4b70fe060063d69655e18c9fc5
Léo MOREAU <thecreepfire@gmail.com>
2024-11-15 16:09:16 +0100

    feat: added local fonts and symbols + removed google import

--- components/ADatePicker.vue
+++ components/ADatePicker.vue
@@ -240,31 +240,16 @@ const dayNames = computed(() => {
 
 <template>
   <div class="a-date-picker">
-    <VueDatePicker
-      v-model="date"
-      :auto-apply="!hasValidation"
-      :format="format"
-      :placeholder="dateTranslate"
-      :cancelText="cancelTranslate"
-      :selectText="validateTranslate"
-      :day-names="dayNames"
-      time-picker-inline
-      :enable-time-picker="hasTime"
-      :range="type === 'range'"
-      :class="{ 'type-range': type === 'range', multi: type === 'range' }"
-      :multi-calendars="type === 'range'"
+    <VueDatePicker v-model="date" :auto-apply="!hasValidation" :format="format" :placeholder="dateTranslate"
+      :cancelText="cancelTranslate" :selectText="validateTranslate" :day-names="dayNames" time-picker-inline
+      :enable-time-picker="hasTime" :range="type === 'range'"
+      :class="{ 'type-range': type === 'range', multi: type === 'range' }" :multi-calendars="type === 'range'"
       :action-row="{
         showSelect: hasValidation,
         showCancel: hasValidation,
-      }"
-    />
+      }" />
   </div>
 </template>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Jacquarda+Bastarda+9&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
-</style>
-
 <style lang="scss">
 .a-date-picker {
   .dp__main {
